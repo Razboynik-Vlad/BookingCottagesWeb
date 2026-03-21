21dc1ab909289e7887ff91e0909d61518057fd18
Valkazavr <v.i.hozeev@gmail.com>
2026-03-21 06:58:02 +0300

    .js updated3

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // На любой запрос, который не является API, отдаем index.html
-app.get('/*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
